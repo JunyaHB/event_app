1e3ae66e2df1566a0a68f6e0495701e4728f648b
EC2 Default User <ec2-user@ip-172-31-18-212.us-east-2.compute.internal>
2019-08-12 17:29:04 +0000

    3

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,20 @@
 class EventsController < ApplicationController
+
   def index
+    @events = Event.all.order(created_at: :desc)
   end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
+
+  def new
+  end
+  
+  def create
+    @event = Event.new(event_name: params[:event_name], date: params[:date], content: params[:content])
+    @event.save
+    redirect_to("/events/index")  
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'events/index' => "events#index"
-
-  get 'home/about' => "home#about"
+  get 'about' => "home#about"
   get "/" => "home#top"
+  get "events/new" => "events#new"
+  get "events/:id" => "events#show"
+  
+  post "events/create" => "events/create"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
