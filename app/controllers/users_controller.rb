6b6c2e897b84e762bc75025c376f2cd1c2193af4
EC2 Default User <ec2-user@ip-172-31-18-212.us-east-2.compute.internal>
2019-08-13 10:19:09 +0000

    7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(name: params[:name], email: params[:email], introduction: params[:introduction])
+    @user = User.new(name: params[:name], 
+    email: params[:email], 
+    introduction: params[:introduction],
+    image_name: "default_user.png"
+    )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
@@ -21,4 +25,29 @@ class UsersController < ApplicationController
       render("users/new")
     end
   end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.introduction = params[:introduction]
+    
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+    
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
+
 end
