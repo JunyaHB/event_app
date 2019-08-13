06c6a57888742ec8968ffa9ab927823d13182ce6
EC2 Default User <ec2-user@ip-172-31-18-212.us-east-2.compute.internal>
2019-08-13 14:35:02 +0000

    9

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
     validates :event_name, {presence: true, length: {maximum: 25}}  
     validates :date, {presence: true}  
     validates :content, {presence: true, length: {maximum: 500}}  
+
+    def user
+        return User.find_by(id: self.user_id)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     validates :name, {presence: true, length: {maximum: 10}}  
     validates :introduction, {length: {maximum: 400}}  
+    
+def events
+    return Event.where(user_id: self.id)
+end
+
+    
 end
